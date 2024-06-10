aed26b2c025703e3ed3790a9442d78ffa18c3ded
omkarg01 <omkargujja01@gmail.com>
2024-06-10 12:17:20 +0530

    database connection done

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import { log } from "console";
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("DB is already connected!");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB connected succesfuly!");
+  } catch (error) {
+    console.log("DB coneection failed", error);
+    process.exit(1);
+  }
+}
